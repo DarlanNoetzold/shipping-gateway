2cf3a7b7c34447ea726f61ab0172fc70b2ca8268
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-12 09:35:49 -0300

    Update ShippingRepository.java

--- src/main/java/tech/noetzold/repository/ShippingRepository.java
+++ src/main/java/tech/noetzold/repository/ShippingRepository.java
@@ -1,4 +1,9 @@
 package tech.noetzold.repository;
 
-public class ShippingRepository {
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import tech.noetzold.model.ShippingModel;
+
+@ApplicationScoped
+public class ShippingRepository implements PanacheRepository<ShippingModel> {
 }
