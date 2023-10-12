694b7e062b6fad21fd4735f056515bd9e4001223
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-12 11:47:50 -0300

    Update State.java

--- src/main/java/tech/noetzold/model/enums/State.java
+++ src/main/java/tech/noetzold/model/enums/State.java
@@ -1,4 +1,11 @@
 package tech.noetzold.model.enums;
 
 public enum State {
+    INIT,
+    PREPARING_PACKAGE,
+    SENDING_TO_REGION,
+    SENDING_TO_YOUR_ADDRESS,
+    RETRY,
+    RETURN,
+    DELIVERED
 }
