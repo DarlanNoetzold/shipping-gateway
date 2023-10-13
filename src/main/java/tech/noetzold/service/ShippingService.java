54f497fefda1d483c1d9a63ae6916232e25396f0
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-13 10:05:48 -0300

    Update ShippingService.java

--- src/main/java/tech/noetzold/service/ShippingService.java
+++ src/main/java/tech/noetzold/service/ShippingService.java
@@ -1,4 +1,13 @@
 package tech.noetzold.service;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import tech.noetzold.repository.ShippingRepository;
+
+@ApplicationScoped
 public class ShippingService {
+
+    @Inject
+    ShippingRepository shippingRepository;
+
 }
