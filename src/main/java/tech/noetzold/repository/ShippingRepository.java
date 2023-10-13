93e719fa64fb37383d897561cef62c27fe5fb58d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-13 10:25:56 -0300

    Update ShippingRepository.java

--- src/main/java/tech/noetzold/repository/ShippingRepository.java
+++ src/main/java/tech/noetzold/repository/ShippingRepository.java
@@ -4,6 +4,17 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import tech.noetzold.model.ShippingModel;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @ApplicationScoped
 public class ShippingRepository implements PanacheRepository<ShippingModel> {
+
+    public Optional<ShippingModel> findByIdOptional(UUID id) {
+        return find("shippingId", id).firstResultOptional();
+    }
+
+    public void deleteById(UUID id) {
+        delete("shippingId", id);
+    }
 }
