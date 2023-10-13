3c3ee1872dd59a22d040bfe7bd2aa86e2746c831
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-13 11:01:38 -0300

    Update ShippingController.java

--- src/main/java/tech/noetzold/controller/ShippingController.java
+++ src/main/java/tech/noetzold/controller/ShippingController.java
@@ -1,5 +1,13 @@
 package tech.noetzold.controller;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/api/shipping/v1/shipping")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ShippingController
 {
 }
