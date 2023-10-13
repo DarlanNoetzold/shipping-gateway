a55a83f79701081129fa50b7a3b7a0fe9d00b38a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-13 10:05:46 -0300

    Update AddressService.java

--- src/main/java/tech/noetzold/service/AddressService.java
+++ src/main/java/tech/noetzold/service/AddressService.java
@@ -1,4 +1,12 @@
 package tech.noetzold.service;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import tech.noetzold.repository.AddressRepository;
+
+@ApplicationScoped
 public class AddressService {
+
+    @Inject
+    AddressRepository addressRepository;
 }
