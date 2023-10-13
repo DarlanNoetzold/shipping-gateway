43c31a37e73bee94d2b82aac91581543e8678a09
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-13 11:01:35 -0300

    Update AddressController.java

--- src/main/java/tech/noetzold/controller/AddressController.java
+++ src/main/java/tech/noetzold/controller/AddressController.java
@@ -1,4 +1,12 @@
 package tech.noetzold.controller;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/api/shipping/v1/address")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class AddressController {
 }
