267f6ada0b5c09e0768853015fa60b737af357f5
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-15 07:12:33 -0300

    Update AddressConsumer.java

--- src/main/java/tech/noetzold/consumer/AddressConsumer.java
+++ src/main/java/tech/noetzold/consumer/AddressConsumer.java
@@ -1,4 +1,34 @@
 package tech.noetzold.consumer;
 
+import io.smallrye.reactive.messaging.annotations.Blocking;
+import io.smallrye.reactive.messaging.annotations.Merge;
+import io.vertx.core.json.JsonObject;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.noetzold.model.AddressModel;
+import tech.noetzold.service.AddressService;
+
+@ApplicationScoped
 public class AddressConsumer {
+
+    @Inject
+    AddressService shippingService;
+
+    private static final Logger logger = LoggerFactory.getLogger(AddressConsumer.class);
+
+    @Incoming("addresses")
+    @Merge
+    @Blocking
+    public AddressModel process(JsonObject incomingAddressModelInJson) {
+
+        AddressModel incomingAddressModel = incomingAddressModelInJson.mapTo(AddressModel.class);
+
+        shippingService.saveAddressModel(incomingAddressModel);
+        logger.info("Create Address " + incomingAddressModel.getAddressId() + ".");
+
+        return incomingAddressModel;
+    }
 }
