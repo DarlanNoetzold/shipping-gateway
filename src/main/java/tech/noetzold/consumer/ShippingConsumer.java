dd8eced78af55bdf09c2d5e7c058907b1a305766
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-15 07:12:35 -0300

    Update ShippingConsumer.java

--- src/main/java/tech/noetzold/consumer/ShippingConsumer.java
+++ src/main/java/tech/noetzold/consumer/ShippingConsumer.java
@@ -1,4 +1,34 @@
 package tech.noetzold.consumer;
 
+import io.smallrye.reactive.messaging.annotations.Blocking;
+import io.smallrye.reactive.messaging.annotations.Merge;
+import io.vertx.core.json.JsonObject;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.noetzold.model.ShippingModel;
+import tech.noetzold.service.ShippingService;
+
+@ApplicationScoped
 public class ShippingConsumer {
+
+    @Inject
+    ShippingService shippingService;
+
+    private static final Logger logger = LoggerFactory.getLogger(ShippingConsumer.class);
+
+    @Incoming("shippings")
+    @Merge
+    @Blocking
+    public ShippingModel process(JsonObject incomingShippingModelInJson) {
+
+        ShippingModel incomingShippingModel = incomingShippingModelInJson.mapTo(ShippingModel.class);
+
+        shippingService.saveShippingModel(incomingShippingModel);
+        logger.info("Create Shipping " + incomingShippingModel.getShippingId() + ".");
+
+        return incomingShippingModel;
+    }
 }
