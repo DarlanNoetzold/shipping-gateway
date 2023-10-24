71001998763639496c6a02a8ee5bf4936df3867a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-24 13:36:45 -0300

    Update AddressRepository.java

--- src/main/java/tech/noetzold/repository/AddressRepository.java
+++ src/main/java/tech/noetzold/repository/AddressRepository.java
@@ -4,6 +4,7 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import tech.noetzold.model.AddressModel;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,5 +17,9 @@ public class AddressRepository implements PanacheRepository<AddressModel> {
 
     public void deleteById(UUID id) {
         delete("addressId", id);
+    }
+
+    public List<AddressModel> findByUserId(String userId) {
+        return list("userId", userId);
     }
 }
