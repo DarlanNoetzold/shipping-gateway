49aae5bda07d5ba0ed089eb77efbacfe5aa4c6bd
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-25 22:19:41 -0300

    Update AddressControllerTest.java

--- src/test/java/tech/noetzold/AddressControllerTest.java
+++ src/test/java/tech/noetzold/AddressControllerTest.java
@@ -1,4 +1,43 @@
 package tech.noetzold;
 
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import tech.noetzold.controller.ShippingController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+@TestHTTPEndpoint(ShippingController.class)
 public class AddressControllerTest {
+
+    private String accessToken;
+
+    @BeforeEach
+    public void obtainAccessToken() {
+        final String username = "admin";
+        final String password = "admin";
+
+        final String tokenEndpoint = "http://localhost:8180/realms/quarkus/protocol/openid-connect/token";
+
+        final Map<String, String> requestData = new HashMap<>();
+        requestData.put("username", username);
+        requestData.put("password", password);
+        requestData.put("grant_type", "password");
+
+        final Response response = given()
+                .auth().preemptive().basic("backend-service", "secret")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .formParams(requestData)
+                .when()
+                .post(tokenEndpoint);
+
+        response.then().statusCode(200);
+
+        this.accessToken = response.jsonPath().getString("access_token");
+    }
 }
